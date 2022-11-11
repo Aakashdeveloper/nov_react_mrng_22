d2c2665a399b7a8a6fc878e199a88d677d111c9a
aakashdeveloper <ahanda205@gmail.com>
2022-11-11 09:53:47 +0530

    reactstart

---
+++ firstapp/src/component/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div>
+            <center>
+                <h1>React Application</h1>
+            </center>
+            <hr/>
+        </div>
+    )
+}
+
+export default Header;
