6c4ee979324d4938cd942ad2aac29527b2e2e940
aakashdeveloper <ahanda205@gmail.com>
2022-12-07 09:53:52 +0530

    routing5

---
+++ routing6/src/component/Profile.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Profile = () => {
+    return(
+        <>
+            <div className="panel panel-danger">
+                <div className="panel-heading">
+                    Profile
+                </div>
+                <div className="panel-body">
+                    <h2>Profile Page</h2>
+                    <p>
+                        Profile is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Profile;
--- routing6/src/component/Routing.js
+++ routing6/src/component/Routing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter,Routes, Route} from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import Home from './Home';
@@ -13,10 +13,12 @@ const Routing = () => {
         <BrowserRouter>
             <Header/>
             <div className="container">
-                <Route exact path="/" component={Home}/>
-                <Route exact path="/post" component={Post}/>
-                <Route path="/post/:topic" component={PostDetails}/>
-                <Route path="/profile" component={Profile}/>
+                <Routes>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/post" component={Post}/>
+                    <Route path="/post/:topic" component={PostDetails}/>
+                    <Route path="/profile" component={Profile}/>
+                </Routes>
             </div>
             <Footer/>
         </BrowserRouter>
