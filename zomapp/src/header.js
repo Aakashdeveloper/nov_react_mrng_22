3e8fb1b20e1e0427ea4b8d47e3a18b3267da2863
aakashdeveloper <ahanda205@gmail.com>
2022-12-27 09:50:51 +0530

    loginwithgit

--- zomapp/src/header.js
+++ zomapp/src/header.js
@@ -8,36 +8,58 @@ class Header extends Component {
         super(props)
 
         this.state={
-            userData:''
+            userData:'',
+            username:'',
+            userImg:''
         }
     }
 
     hanldeLogout = () => {
         sessionStorage.removeItem('userInfo')
         sessionStorage.removeItem('ltk')
+        sessionStorage.removeItem('uName');
+        sessionStorage.removeItem('uImg');
         sessionStorage.setItem('loginStatus','LoggedOut')
-        this.setState({userData:''})
+        this.setState({userData:'',username:'',userImg:''})
         this.props.history.push('/')
     }
 
     conditionalRender = () => {
-        if(this.state.userData.name){
-            let data = this.state.userData;
-            sessionStorage.setItem('userInfo',JSON.stringify(data));
-            sessionStorage.setItem('loginStatus','LoggedIn')
-            return(
+        if(this.state.userData.name || sessionStorage.getItem('uName') !== null){
+            if(sessionStorage.getItem('uName') !== null){
+                let name = sessionStorage.getItem('uName');
+                let image = sessionStorage.getItem('uImg');
                 <>
                     <Link className="btn btn-info" to="/">
-                        <span className='glyphicon glyphicon-user'></span> Hi {data.name}
+                        Hi <img src={image} style={{height:50,width:50}}/> {name}
                     </Link> &nbsp;
                     <button className="btn btn-danger" onClick={this.hanldeLogout}>
                         <span className='glyphicon glyphicon-log-out'></span> Logout
                     </button>
                 </>
-            )
-        }else{
+            }
+            else{
+                let data = this.state.userData;
+                sessionStorage.setItem('userInfo',JSON.stringify(data));
+                sessionStorage.setItem('loginStatus','LoggedIn')
+                return(
+                    <>
+                        <Link className="btn btn-info" to="/">
+                            <span className='glyphicon glyphicon-user'></span> Hi {data.name}
+                        </Link> &nbsp;
+                        <button className="btn btn-danger" onClick={this.hanldeLogout}>
+                            <span className='glyphicon glyphicon-log-out'></span> Logout
+                        </button>
+                    </>
+                )
+            }
+        }
+        else{
             return(
-                <>                
+                <>    
+                    <a className="btn btn-info" href="https://github.com/login/oauth/authorize?client_id=930f92e500db2f4d357c">
+                        Login With Github
+                    </a> &nbsp;            
                     <Link className="btn btn-success" to="/login">Login</Link> &nbsp;
                     <Link className="btn btn-danger" to="/register">Register</Link>
                 </>
@@ -63,18 +85,48 @@ class Header extends Component {
 
 
     componentDidMount(){
-        fetch(url,{
-            method:'GET',
-            headers:{
-                'x-access-token':sessionStorage.getItem('ltk')
+        if(this.props.location.search){
+            if(this.props.location.search.split('=')[0] === '?code'){
+                var code = this.props.location.search.split('=')[0][1]
             }
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            this.setState({
-                userData:data
+            if(code){
+                let requestedData = {
+                    code:code
+                }
+                fetch(`http://localhost:9900/oauth`,{
+                    method: 'POST',
+                    headers:{
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body:JSON.stringify(requestedData)
+                })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log(data)
+                    let username = data.name;
+                    let img = data.avatar_url;
+                    sessionStorage.setItem('uName',username);
+                    sessionStorage.setItem('uImg',img);
+                    sessionStorage.setItem('loginStatus','LoggedIn')
+                    this.setState({username:username,userImg:img})
+                })
+            }
+        }else{
+            fetch(url,{
+                method:'GET',
+                headers:{
+                    'x-access-token':sessionStorage.getItem('ltk')
+                }
             })
-        })
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState({
+                    userData:data
+                })
+            })
+        }
+        
     }
 
 
